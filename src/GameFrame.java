1e605a17132670c1e1ebdc858807b4f31eecf89e
aoitchan0212 <aoi.t.chan0212@gmail.com>
2025-06-16 12:00:45 +0900

    10章

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,4 +1,35 @@
+import java.util.Vector;
 
 public class GameFrame extends MyFrame{
+	public void run() {
+		GameWorld.player = new Player(100,300,0,0);
+		addKeyListener(GameWorld.player);
+		GameWorld.playerBullets = new Vector<PlayerBullet>();
+		while(true) {
+			clear();
+			GameWorld.player.draw(this);
+			GameWorld.player.move();
+			movePlayerBullets();
+			sleep(0.03);
+		}
+	}
+	
+	//弾を発射するメゾット
+	public void movePlayerBullets() {
+		int i=0;
+		while (i<GameWorld.playerBullets.size()) {
+			PlayerBullet b=GameWorld.playerBullets.get(i);
+			b.draw(this);
+			b.move();
+			
+			//弾の縦座標<0で削除
+			if(b.y<0) {
+				GameWorld.playerBullets.remove(i);
+			}
+			else {
+				i++;
+			}
+		}
+	}
 
 }
--- src/PlayerBullet.java
+++ src/PlayerBullet.java
@@ -1,4 +1,14 @@
 
 public class PlayerBullet extends Character{
+	public void draw(MyFrame f) {
+		f.setColor(0, 128, 0);
+		f.fillRect(x+10, y, 10, 30);
+	}
+	
+	
+	public PlayerBullet(double x, double y, double vx, double vy) {
+		super(x,y,vx,vy);
+	}
 
 }
+ 
