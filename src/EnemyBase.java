026057d4a11c2e68e2cc74fc6b31a674463036c6
aoitchan0212 <aoi.t.chan0212@gmail.com>
2025-06-23 10:31:13 +0900

    うーたん完成

--- src/EnemyBase.java
+++ src/EnemyBase.java
@@ -14,9 +14,11 @@ public class EnemyBase extends Enemy{
 		f.fillOval(x-16, y+8, 64, 16);
 	}
 	
-	//敵が右端に来たら反対方向に移動
+	
 	public void move() {
 		super.move();
+		
+		//敵が右端に来たら反対方向に移動
 		if(x>300) vx=-GameWorld.stage;
 		if(x<100) vx=GameWorld.stage;
 		
@@ -25,12 +27,12 @@ public class EnemyBase extends Enemy{
 			GameWorld.enemies.add(new StraightEnemy(x,y,0,1+GameWorld.stage));
 			
 		//敵(ランダム)	
-		if(Math.random()<0.4) {
+		if(Math.random()<0.01) {
 			GameWorld.enemies.add(new RandomEnemy(x,y,0,GameWorld.stage));
 		}
 		
 		//敵(縦に加速)
-		if(Math.random()<0.3) {
+		if(Math.random()<0.03) {
 			GameWorld.enemies.add(new DropEnemy(x,y,0,GameWorld.stage));
 		}
 		
@@ -40,8 +42,8 @@ public class EnemyBase extends Enemy{
 		}
 		
 		//敵(うーたん)	
-		if(Math.random()<0.4) {
-					GameWorld.enemies.add(new OriginalEnemy(x,y,0,GameWorld.stage));
+		if(Math.random()<0.9) {
+			GameWorld.enemies.add(new OriginalEnemy(x,y,0,GameWorld.stage));
 		}
 		
 		}
--- src/OriginalEnemy.java
+++ src/OriginalEnemy.java
@@ -7,41 +7,50 @@ public class OriginalEnemy extends Enemy{
 	}
 
 	public void draw(MyFrame f) {
-		
+	
 		// 頭の色（クリーム色）
 		f.setColor(255, 253, 215);  // やさしいクリーム
-		f.fillOval(100, 100, 30, 30);  // 小さな頭
+		f.fillOval(x, y, 30, 30);  // 小さな頭
 
 		// ほっぺ
 		f.setColor(255, 170, 180);  // ピンク
-		f.fillOval(105, 115, 5, 5);  // 左ほっぺ
-		f.fillOval(120, 115, 5, 5);  // 右ほっぺ
+		f.fillOval(x + 5, y + 15, 5, 5);  // 左ほっぺ
+		f.fillOval(x + 20, y + 15, 5, 5);  // 右ほっぺ
 
 		// 目
 		f.setColor(0, 0, 0);  // 黒
-		f.fillOval(106, 108, 5, 7);  // 左目
-		f.fillOval(119, 108, 5, 7);  // 右目
+		f.fillOval(x + 6, y + 8, 5, 7);  // 左目
+		f.fillOval(x + 19, y + 8, 5, 7);  // 右目
 
 		// ハイライト
 		f.setColor(255, 255, 255);  // 白
-		f.fillOval(107, 109, 2, 2);  // 左目ハイライト
-		f.fillOval(120, 109, 2, 2);  // 右目ハイライト
+		f.fillOval(x + 7, y + 9, 2, 2);  // 左目ハイライト
+		f.fillOval(x + 20, y + 9, 2, 2);  // 右目ハイライト
 
 		// 口
 		f.setColor(255, 100, 120);  // うっすら赤
-		f.fillOval(112, 118, 6, 3);  // 小さい口
-		
+		f.fillOval(x + 12, y + 18, 6, 3);  // 小さい口
+
 		// --- マラカス（左） ---
 		f.setColor(255, 130, 0);  // オレンジ
-		f.fillOval(88, 88, 10, 10); 
-		f.fillOval(95, 95, 6, 6);    
+		f.fillOval(x - 7, y - 7, 10, 10); 
+		f.fillOval(x, y, 5, 5);    
 
 		// --- マラカス（右） ---
-		f.fillOval(132, 88, 10, 10);  
-		f.fillOval(129, 95, 6, 6);  
-
-		
+		f.fillOval(x + 29, y - 7, 10, 10);  
+		f.fillOval(x + 26, y, 5, 5);  
 	}
+	
+	public void move() {
+		super.move();
+		if(x<GameWorld.player.x) {
+			x++;     //自分がﾌﾟﾚｲﾔｰより左なら右に移動
+		}
+		if(x>GameWorld.player.y) {
+			x--;     //自分がﾌﾟﾚｲﾔｰより右なら左に移動
+		}
+	}
+	
 
 	
 }
